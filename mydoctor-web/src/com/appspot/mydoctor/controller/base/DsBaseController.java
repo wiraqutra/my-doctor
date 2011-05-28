4a4b61225e1fd1459ab3cddb4e7b8cb64c9320fa
kana2ggl <kana2ggl@gmail.com>
2011-05-28 08:33:15 +0000

    add ds

---
+++ mydoctor-web/src/com/appspot/mydoctor/controller/base/DsBaseController.java
@@ -0,0 +1,9 @@
+package com.appspot.mydoctor.controller.base;
+
+public abstract class DsBaseController extends BaseController {
+
+    @Override
+    protected String getJspDir() {
+        return "/jsp/ds/";
+    }
+}
