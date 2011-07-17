a2bbc87e2b50f90947d176e647869369ce7e4a36
kana2ggl <kana2ggl@gmail.com>
2011-07-17 03:55:25 +0000

    modify session id parameter

--- mydoctor-web/src/com/appspot/mydoctor/controller/callback/TwController.java
+++ mydoctor-web/src/com/appspot/mydoctor/controller/callback/TwController.java
@@ -8,6 +8,7 @@ import com.appspot.mydoctor.controller.base.PcBaseController;
 import com.appspot.mydoctor.meta.TwitterAuthSessionModelMeta;
 import com.appspot.mydoctor.model.TwitterAuthSessionModel;
 import com.appspot.mydoctor.service.UserAccountService;
+import com.appspot.mydoctor.util.UrlUtil;
 
 public class TwController extends PcBaseController {
 
@@ -15,13 +16,16 @@ public class TwController extends PcBaseController {
 	public Navigation run() throws Exception {
 		String verifier = requestScope("oauth_verifier");
 		String sessionKey = requestScope("tid");
-		if (StringUtils.isNotEmpty(sessionKey)) {
+		if (StringUtils.isNotEmpty(sessionKey) && StringUtils.isNotEmpty(verifier)) {
 			TwitterAuthSessionModelMeta meta = TwitterAuthSessionModelMeta.get();
 			TwitterAuthSessionModel tas = Datastore.query(meta).filter(meta.sessionKey.equal(sessionKey)).asSingle();
 			if (tas != null) {
 				UserAccountService uas = new UserAccountService();
 				String skey = uas.createLoginSession(tas, verifier);
 				requestScope("sessionKey", skey);
+				if (StringUtils.isNotEmpty(tas.getRedirectUrl())) {
+					return redirect(UrlUtil.addSessionId(tas.getRedirectUrl(), skey));
+				}
 			}
 		}
 		return forward("callback/tw.jsp");
